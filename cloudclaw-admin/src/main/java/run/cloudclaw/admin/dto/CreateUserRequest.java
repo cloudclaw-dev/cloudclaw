0c96fe3fcdb37d69cbdfdd36b9a8e6bf7f5047ed
cloudclaw-dev <cloudclaw-dev@zhaoyanblog.com>
2026-05-21 11:27:13 +0800

    Add cloudclaw-admin/src/main/java/run/cloudclaw/admin/dto/CreateUserRequest.java

---
+++ cloudclaw-admin/src/main/java/run/cloudclaw/admin/dto/CreateUserRequest.java
@@ -0,0 +1,32 @@
+package run.cloudclaw.admin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 100, message = "Username must be between 3 and 100 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    @Size(max = 200, message = "Email must be at most 200 characters")
+    private String email;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+}
