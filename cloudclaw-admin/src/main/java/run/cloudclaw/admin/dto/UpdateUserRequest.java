fcf693c06780d58d5e4ceb55048ac660fe655feb
cloudclaw-dev <cloudclaw-dev@zhaoyanblog.com>
2026-05-21 11:27:16 +0800

    Add cloudclaw-admin/src/main/java/run/cloudclaw/admin/dto/UpdateUserRequest.java

---
+++ cloudclaw-admin/src/main/java/run/cloudclaw/admin/dto/UpdateUserRequest.java
@@ -0,0 +1,23 @@
+package run.cloudclaw.admin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+    @Email(message = "Email must be a valid email address")
+    @Size(max = 200, message = "Email must be at most 200 characters")
+    private String email;
+
+    private String role;
+
+    private Boolean enabled;
+}
