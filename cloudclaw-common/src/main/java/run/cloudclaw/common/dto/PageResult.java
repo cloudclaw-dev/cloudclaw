6500d3aef7ec38576144c55ad8802cd2e0793a8e
cloudclaw-dev <cloudclaw-dev@zhaoyanblog.com>
2026-05-21 11:28:26 +0800

    Add cloudclaw-common/src/main/java/run/cloudclaw/common/dto/PageResult.java

---
+++ cloudclaw-common/src/main/java/run/cloudclaw/common/dto/PageResult.java
@@ -0,0 +1,24 @@
+package run.cloudclaw.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResult<T> {
+
+    private List<T> list;
+    private long total;
+    private int page;
+    private int size;
+
+    public static <T> PageResult<T> of(List<T> list, long total, int page, int size) {
+        return new PageResult<>(list, total, page, size);
+    }
+}
