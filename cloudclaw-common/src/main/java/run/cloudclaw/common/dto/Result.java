f82338b14cbeb61a7303961318c3e2b77d6234d6
cloudclaw-dev <cloudclaw-dev@zhaoyanblog.com>
2026-05-21 11:28:30 +0800

    Add cloudclaw-common/src/main/java/run/cloudclaw/common/dto/Result.java

---
+++ cloudclaw-common/src/main/java/run/cloudclaw/common/dto/Result.java
@@ -0,0 +1,33 @@
+package run.cloudclaw.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> Result<T> ok(T data) {
+        return new Result<>(200, "success", data);
+    }
+
+    public static <T> Result<T> ok() {
+        return new Result<>(200, "success", null);
+    }
+
+    public static <T> Result<T> error(int code, String message) {
+        return new Result<>(code, message, null);
+    }
+
+    public static <T> Result<T> error(String message) {
+        return new Result<>(500, message, null);
+    }
+}
